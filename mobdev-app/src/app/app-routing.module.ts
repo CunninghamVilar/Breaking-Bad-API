fabb861f0ec2f63e6130e66b8dfc17a862c49b03
CunninghamVilar <villar885@gmail.com>
2020-04-16 17:35:37 +0000

    update

--- mobdev-app/src/app/app-routing.module.ts
+++ mobdev-app/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
 { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
@@ -12,3 +13,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
