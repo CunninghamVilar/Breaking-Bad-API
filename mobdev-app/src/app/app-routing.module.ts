ade632fa86e132220a0016418a1950eb6e1aca12
CunninghamVilar <villar885@gmail.com>
2020-04-19 11:46:13 +0000

    Created new pages

--- mobdev-app/src/app/app-routing.module.ts
+++ mobdev-app/src/app/app-routing.module.ts
@@ -3,7 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
-{ path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
+{ path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)},
+  {
+    path: 'character-details',
+    loadChildren: () => import('./pages/character-details/character-details.module').then( m => m.CharacterDetailsPageModule)
+  },
+  {
+    path: 'episode-details',
+    loadChildren: () => import('./pages/episode-details/episode-details.module').then( m => m.EpisodeDetailsPageModule)
+  },
+  {
+    path: 'quote-details',
+    loadChildren: () => import('./pages/quote-details/quote-details.module').then( m => m.QuoteDetailsPageModule)
+  }
 ];
 
 @NgModule({
