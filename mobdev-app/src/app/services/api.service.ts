c7f138db7fae5933454a1fc00b04f0f269c871df
CunninghamVilar <villar885@gmail.com>
2020-04-19 13:42:28 +0000

    Api

--- mobdev-app/src/app/services/api.service.ts
+++ mobdev-app/src/app/services/api.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
 
-  constructor() { }
+export class ApiService 
+
+{
+  constructor(private http: HttpClient) { }
+  getEpisodes() 
+  {
+      return this.http.get(`https://www.breakingbadapi.com/api/episodes`)
+  }
+  getEpisode(id) 
+  {
+      return this.http.get(`https://breakingbadapi.com/api/episodes/${id}`)
+  }
+  
 }
+
+
