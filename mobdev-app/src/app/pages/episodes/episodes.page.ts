cdbdb2abf3941aa2676093d3e500379a7e0e0ab4
CunninghamVilar <villar885@gmail.com>
2020-04-19 14:05:12 +0000

    episode details updated

--- mobdev-app/src/app/pages/episodes/episodes.page.ts
+++ mobdev-app/src/app/pages/episodes/episodes.page.ts
@@ -1,29 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
-
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
 @Component({
     selector: 'app-episodes',
     templateUrl: './episodes.page.html',
     styleUrls: ['./episodes.page.scss'],
 })
 export class EpisodesPage implements OnInit {
-
-    constructor(private navController: NavController, private router: Router) { }
-
+    episodes: Observable<any>;
+    constructor(private router: Router, private api: ApiService) { }
     ngOnInit() {
+        this.episodes = this.api.getEpisodes();
+        this.episodes.subscribe(data => {
+            console.log('my data: ', data);
+        });
     }
-
-    openDetails() {
-        // Both of these would work!
-        // But the standard Router is recommended.
-        // this.navController.navigateForward(`/tabs/films/42`);
-        this.router.navigateByUrl(`/tabs/episodes/42`);
+    openDetails(episode) {
+        let episodeId = episode.episode_id;
+        this.router.navigateByUrl(`/tabs/episodes/${episodeId}`);
     }
-
-    goToEpisodes() {
-        this.navController.navigateRoot(`/tabs/episodes`)
-    }
-
 }
