3d0f4716c21485b54b0d6ea739f08e0228c3c0e2
CunninghamVilar <villar885@gmail.com>
2020-04-19 12:15:35 +0000

    API routing updated

--- mobdev-app/src/app/app-routing.module.ts
+++ mobdev-app/src/app/app-routing.module.ts
@@ -2,20 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
+
+
 const routes: Routes = [
-{ path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)},
-  {
-    path: 'character-details',
-    loadChildren: () => import('./pages/character-details/character-details.module').then( m => m.CharacterDetailsPageModule)
-  },
-  {
-    path: 'episode-details',
-    loadChildren: () => import('./pages/episode-details/episode-details.module').then( m => m.EpisodeDetailsPageModule)
-  },
-  {
-    path: 'quote-details',
-    loadChildren: () => import('./pages/quote-details/quote-details.module').then( m => m.QuoteDetailsPageModule)
-  }
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
 ];
 
 @NgModule({
