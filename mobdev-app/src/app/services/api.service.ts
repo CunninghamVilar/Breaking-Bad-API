0ea1b3ffce30b8e8d6e8ca09ebec551e0db9200d
CunninghamVilar <villar885@gmail.com>
2020-04-24 17:14:56 +0000

    changes

--- mobdev-app/src/app/services/api.service.ts
+++ mobdev-app/src/app/services/api.service.ts
@@ -7,24 +7,48 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
     constructor(private http: HttpClient) { }
 
-    getEpisodes() 
-    {
-        return this.http.get(`https://www.breakingbadapi.com/api/episodes`)
+    //getEpisodes() 
+    //{
+    //return this.http.get(`https://www.breakingbadapi.com/api/episodes`)
+    // }
+
+    //getEpisode(id) 
+    //{
+    //return this.http.get(`https://www.breakingbadapi.com/api/episodes/${id}`)
+    // }
+    getEpisodes() {
+        return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/episodes.json`)
     }
 
-    getEpisode(id) 
-    {
-        return this.http.get(`https://breakingbadapi.com/api/episodes/${id}`)
+    getEpisode(id) {
+        return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/episode.json`)
     }
 
-    getCharacters() 
-    {
-        return this.http.get(`https://www.breakingbadapi.com/api/characters`)
+
+    //getCharacters() 
+    //{
+    //return this.http.get(`https://www.breakingbadapi.com/api/characters`)
+    //}
+
+    //getCharacter(id) 
+    //{
+    //return this.http.get(`https://breakingbadapi.com/api/characters/${id}`)
+    //}
+    getCharacters() {
+        return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/characters.json`)
     }
-    
-    getCharacter(id) 
-    {
-        return this.http.get(`https://breakingbadapi.com/api/characters/${id}`)
+
+    getCharacter(id) {
+        return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/character.json`)
+    }
+
+
+    findQuote(search: any) {
+
+        return this.http.get(`http://breakingbadapi.com/api/quote?author=${search}`).pipe(new Map( quote => {
+            return quote;
+        })
+        
     }
 
 }
