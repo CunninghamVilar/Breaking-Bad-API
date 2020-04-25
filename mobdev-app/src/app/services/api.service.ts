33ff99c357bf6778f8b6b2cecc8af9dbc06c9621
CunninghamVilar <villar885@gmail.com>
2020-04-25 10:47:37 +0000

    Back to use API link Breaking bad instead of copied API.

--- mobdev-app/src/app/services/api.service.ts
+++ mobdev-app/src/app/services/api.service.ts
@@ -14,57 +14,66 @@ export class ApiService {
     
     constructor(private http: HttpClient) { }
 
-    //getEpisodes() 
-    //{
-    //return this.http.get(`https://www.breakingbadapi.com/api/episodes`)
-    // }
-
-    //getEpisode(id) 
-    //{
-    //return this.http.get(`https://www.breakingbadapi.com/api/episodes/${id}`)
-    // }
-    getEpisodes() {
-        return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/episodes.json`)
+    getEpisodes() 
+    {
+    return this.http.get(`https://www.breakingbadapi.com/api/episodes`)
     }
 
-    getEpisode(id) {
-        return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/episode.json`)
+    getEpisode(id: any) 
+    {
+    return this.http.get(`https://www.breakingbadapi.com/api/episodes/${id}`)
+    }
+
+    //getEpisodes() {
+    //    return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/episodes.json`)
+    //}
+
+    //getEpisode(id) {
+    //    return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/episode.json`)
+    //}
+
+    getCharacters() 
+    {
+    return this.http.get(`https://www.breakingbadapi.com/api/characters`)
+    }
+
+    getCharacter(id: any) 
+    {
+    return this.http.get(`https://breakingbadapi.com/api/characters/${id}`)
     }
 
     //getCharacters() 
     //{
-    //eturn this.http.get(`https://www.breakingbadapi.com/api/characters`)
+    // return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/characters.json`)
+    // }
+
+    //getCharacter(id) {
+        //return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/character.json`)
     //}
-
-    //getCharacter(id) 
-    //{
-    //return this.http.get(`https://breakingbadapi.com/api/characters/${id}`)
-    //}
-
-    getCharacters() {
-        return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/characters.json`)
-    }
-
-    getCharacter(id) {
-        return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/character.json`)
-    }
-
 
     getQuotes() {
-        return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/quotes.json`)
+        return this.http.get(`https://www.breakingbadapi.com/api/quotes`)
     }
 
-    getQuote(id) {
-        return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/quote.json`)
+    getQuote(id: any) {
+        return this.http.get(`https://www.breakingbadapi.com/api/quotes/${id}`)
     }
+
+    //getQuotes() {
+    //    return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/quotes.json`)
+    //}
+
+    //getQuote(id) {
+    //    return this.http.get(`https://4200-f239c721-da99-478a-bc62-a24f9b2a0a06.ws-eu01.gitpod.io/assets/quote.json`)
+    //}
 
 
 
     //findQuote(search: any) {
 
-        //return this.http.get(`http://breakingbadapi.com/api/quote?author=${search}`).pipe(new Map( quote => {
-            //return quote;
-        //})
+        //return this.http.get(`http://breakingbadapi.com/api/quote?author=${search}`).pipe(new Map( (quote: any) => {
+    //        return quote;
+    //    })
         
     //}
 
